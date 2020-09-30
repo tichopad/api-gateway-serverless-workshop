5f75fc9a16246e7500140386b8271aeb5d4b32c4
Michael Tichopad <michaeltichopad01@gmail.com>
2020-09-30 16:16:00 +0200

    Week 1 exercises

--- functions/get-index.js
+++ functions/get-index.js
@@ -5,6 +5,10 @@ const aws4 = require("aws4");
 const URL = require("url");
 
 const restaurantsApiRoot = process.env.restaurants_api;
+const cognitoUserPoolId = process.env.cognito_user_pool_id;
+const cognitoClientId = process.env.cognito_client_id;
+const awsRegion = process.env.AWS_REGION;
+
 const days = [
   "Sunday",
   "Monday",
@@ -37,7 +41,15 @@ module.exports.handler = async (event, context) => {
   const restaurants = await getRestaurants();
   console.log(`found ${restaurants.length} restaurants`);
   const dayOfWeek = days[new Date().getDay()];
-  const html = Mustache.render(template, { dayOfWeek, restaurants });
+  const view = {
+    awsRegion,
+    cognitoUserPoolId,
+    cognitoClientId,
+    dayOfWeek,
+    restaurants,
+    searchUrl: `${restaurantsApiRoot}/search`,
+  };
+  const html = Mustache.render(template, view);
   const response = {
     statusCode: 200,
     headers: {
